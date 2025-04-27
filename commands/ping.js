0a808f30816001d01aace2bac84d4a6dd549b104
ranbf-netizen <ransbaffoeameyaw90@gmail.com>
2025-04-27 03:38:55 -0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -20,15 +20,7 @@ async function pingCommand(sock, chatId) {
                        `*🔄 Uptime:* ${formatTime(uptime)}\n` +
                        `*💾 RAM Usage:* ${ram.toFixed(2)}GB`;
 
-        await sock.sendMessage(chatId, {
-            text: message,
-            contextInfo: {
-                forwardingScore: 999,
-                isForwarded: true,
-                forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
-                    serverMessageId: -1
+       
                 }
             }
         });
